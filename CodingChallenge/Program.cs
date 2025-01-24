5a7809885eb3ffd4e93d7c56b2291b3bbcb82e4e
Marcus Trigell <marcus.trigell@afry.com>
2025-01-24 17:55:32 +0100

    WIP: first solution commit

---
+++ CodingChallenge/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Linq;
+
+while (true)
+{
+    var directoryPath = "./input-data"; // TODO: get from appsettings
+    var filePaths = Directory.GetFiles(directoryPath);
+    foreach (var filePath in filePaths)
+    {
+        if (!filePath.ToLower().EndsWith(".xml")) continue;
+        
+        var xmlFile = XElement.Load(filePath);
+        
+        // TODO: do stuff with loaded xml
+        
+    }
+}
